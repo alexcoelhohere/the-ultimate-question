3ec36c0fdbc6c8130be678a473e710084c3c3d2b
Alex Coelho <coelhohustle@gmail.com>
2020-07-25 01:09:05 +0530

    Add files via upload

--- snoopJS.js
+++ snoopJS.js
@@ -1,3 +1,16 @@
-function showImage(){
- document.getElementById('snooparrest').style.visibility="visible";
-}
+// name function
+    let Fname = document.getElementById('name_input').value;
+    function getName(){
+    document.getElementById('welcome_text').innerHTML='Welcome,'+Fname;
+    }
+// var who = prompt('who let the dogs out?');
+// function dogs(who) {
+//     var arrest=('Arrest'+' '+who);
+//     console.log(arrest);
+// }
+// dogs(who);
+
+// function rightAnswer(){
+//  document.getElementById('snooparrest').style.visibility="visible";
+//  document.getElementById('snooparrest');
+// }
